d8ca464aac09b76adab5b97070499a3a4e6feb32
btd-hub <abeey4u@gmail.com>
2023-08-18 02:22:19 +0100

    secondcommit

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function Home() {
 <nav class=" flex items-center p-4 sm:p-6 w-full flex-row justify-between z-10">
         <div className="flex">
         <Image src='src\CD.png' alt='CoinDispute' 
-            boxSize= '60px'
+            boxSize= '80px'
             marginLeft={2}
             />
           
@@ -31,7 +31,7 @@ export default function Home() {
         bgClip='text'
         fontSize='4xl'
         fontWeight='extrabold'>
-        CoinDispute DAO Airdrop
+        $STC Airdrop
     </Text>
   </h1>
 
@@ -40,10 +40,10 @@ export default function Home() {
       <div class="w-full sm:w-1/2 px-4">
         <div class="backdrop-blur-xl rounded-lg shadow-md p-5 bg-opacity-50">
         <center>
-        <Image src='src\emoji.png' alt='Winner'
+        {/* <Image src='src\emoji.png' alt='Winner'
             boxSize= '60px'
             marginLeft={2}
-            />
+            /> */}
             </center>
           <h1 className="text-3xl font-medium text-white text-center">Congratulations!
           <br></br>
@@ -51,21 +51,21 @@ export default function Home() {
           </h1>
           
           <div class="flex justify-between bg-slate-400 bg-opacity-50 rounded-lg py-6 my-6 ">
-            <p class="mr-4 mx-6 text-sm justify-start">You will receive<br/><span class="text-2xl text-green-500 font-bold inline-flex">625,308
+            <p class="mr-4 mx-6 text-sm justify-start">You will receive<br/><span class="text-2xl text-blue-600 font-bold inline-flex">890,308
             <Image src='src\Hash.png' alt='Token' borderRadius='full'
             boxSize= '25px'
             marginLeft={2}
             mt={1}
             />
             </span></p>
-            <button class="bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded-lg mx-6 ">
+            <button class="bg-blue-600 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded-lg mx-6 ">
             Claim
             </button>
           </div>
           
           <hr />
 
-          <h1 class="pt-3 text-white">Excess $HASH should be burned after claim </h1>
+          <h1 class="pt-3 text-white">Excess $STC should be burned after claim </h1>
           <div class="flex justify-start items-center mb-3">
             <p class="mr-4 text-white">Claiming will end in: </p>
             
@@ -76,10 +76,10 @@ export default function Home() {
               </div>
 
               <div class="flex justify-start items-center mb-3">
-            <p class="mr-4 text-white">$HASH for airdrop left: </p>
+            <p class="mr-4 text-white">$STC for airdrop left: </p>
             <center>
             <div class="bg-slate-700  text-slate-300 font-bold py-2 px-16 rounded-lg bg-opacity-50">
-            84,736,229
+            12,336,029
              </div>
              </center>
               </div>
@@ -93,7 +93,7 @@ export default function Home() {
           <ol class="text-white pb-56">
             <li>
             &#43;
-              Ethereum Mainnet
+              Arbitrum Mainnet
             </li>
             <li>
             &#43;
@@ -105,7 +105,7 @@ export default function Home() {
             </li>
             <li>
             &#215;
-            Wrapped Eth
+            Wrapped ARB
             </li>
           </ol>
         </div>
